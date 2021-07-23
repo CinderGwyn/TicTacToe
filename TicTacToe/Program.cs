41c1fc24bd04020796bd44ef8c2316e646088da7
CinderGwyn <87784456+CinderGwyn@users.noreply.github.com>
2021-07-23 19:15:33 -0400

    Removed all unnecessary static use and refactored accordingly.  Added another test for GetMove

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,15 +5,15 @@ namespace TicTacToe
 {
 
     public class Game
-    {   
+    {
         // Fields
-        static char[] board = new char[] { ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; //represents the 9 squares on the board, skipping the 0 element for clarity
-        static string boardDisplay;
-        static char currentPlayer = 'X';
+        char[] board = new char[] { ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; //represents the 9 squares on the board, skipping the 0 element for clarity
+        string boardDisplay;
+        char currentPlayer = 'X';
 
         // Constructors
 
-            // Default
+        // Default
         public Game(char[] inboard, char inCurrentPlayer)
         {
             Array.Copy(inboard, board, 10);
@@ -34,17 +34,17 @@ namespace TicTacToe
 
 
         // Methods
-        static void UpdateDisplay()  //updates the formatted string used for displaying the gameboard to the board array's current state
+        public void UpdateDisplay()  //updates the formatted string used for displaying the gameboard to the board array's current state
         {
-        boardDisplay = String.Format("{0}{1}|{2}|{3}\n-----\n{4}|{5}|{6}\n-----\n{7}|{8}|{9}\n", null, board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8], board[9]);
+            boardDisplay = String.Format("{0}{1}|{2}|{3}\n-----\n{4}|{5}|{6}\n-----\n{7}|{8}|{9}\n", null, board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8], board[9]);
         }
 
-        static void PrintTurn()
+        public void PrintTurn()
         {
             Console.WriteLine($"Player {currentPlayer}'s turn\n");
         }
 
-        static bool IsSingleDigit(string str)
+        public bool IsSingleDigit(string str)
         {
             if (str.Length > 1)
             {
@@ -54,7 +54,7 @@ namespace TicTacToe
             return true;
         }
 
-        public static int GetMove(IConsole console) //Prompts the user for a valid move and re-prompts for every invailid input.  Checks that the input is an integer, single digit, and indicates an unclaimed space on the board.
+        public int GetMove(IConsole console) //Prompts the user for a valid move and re-prompts for every invailid input.  Checks that the input is an integer, single digit, and indicates an unclaimed space on the board.
         {
             console.WriteLine("Enter the number of the square to give your mark:\n");
             var moveAsString = console.ReadLine();
@@ -70,13 +70,13 @@ namespace TicTacToe
             return moveAsInt;
         }
 
-        static void UpdateBoard(int moveInput)  //updates the array containing the states of the board's squares
+        public void UpdateBoard(int moveInput)  //updates the array containing the states of the board's squares
         {
             board[moveInput] = currentPlayer;
             UpdateDisplay();
         }
 
-        static void PrintBoard()
+        public void PrintBoard()
         {
             Console.WriteLine(boardDisplay);
         }
@@ -104,15 +104,16 @@ namespace TicTacToe
 
         static void Main()
         {
-            ConsoleWrapper consoleInput = new ConsoleWrapper();
+            ConsoleWrapper consoleInput = new Game.ConsoleWrapper();
+            Game newGame = new Game();
 
-            UpdateDisplay();
-            PrintBoard();
-            PrintTurn();
-            UpdateBoard(GetMove(consoleInput));
-            PrintBoard();
+            newGame.UpdateDisplay();
+            newGame.PrintBoard();
+            newGame.PrintTurn();
+            newGame.UpdateBoard(newGame.GetMove(consoleInput));
+            newGame.PrintBoard();
         }
-}
+    }
 
 }
 
--- TicTacToeUnitTests/UnitTest1.cs
+++ TicTacToeUnitTests/UnitTest1.cs
@@ -29,7 +29,7 @@ namespace TicTacToeUnitTests
                 return result;
             }
 
-            
+
         }
 
         ConsoleWrapper testInput = new ConsoleWrapper();
@@ -39,24 +39,39 @@ namespace TicTacToeUnitTests
         { //test commit
 
             //Arrange
-            
+            Game testBoard = new Game();
+
             testInput.LinesToRead.Add("9");
             //Act
-            int getMoveTest = TicTacToe.Game.GetMove(testInput);
+            int getMoveTest = testBoard.GetMove(testInput);
             //Assert
             Assert.AreEqual<int>(9, getMoveTest);
         }
         [TestMethod]
-        public void ThirdTimesTheCharm()
+        public void ThirdTimesTheCharm()  // first passess in two invalid entries followed by a third valid one.  GetMove should discard all invalid entries and return the valid.
         {
             //Arrange
+            Game testBoard = new Game();
             testInput.LinesToRead.Add("P");
-            testInput.LinesToRead.Add("G");
+            testInput.LinesToRead.Add("0");
             testInput.LinesToRead.Add("9");
             //Act
-            int getMoveTest = TicTacToe.Game.GetMove(testInput);
+            int getMoveTest = testBoard.GetMove(testInput);
             //Assert
             Assert.AreEqual<int>(9, getMoveTest);
+        }
+        [TestMethod]
+        public void FourthTime()
+        {
+            Game testBoard = new Game();
+            testBoard.UpdateBoard(9);
+            testInput.LinesToRead.Add("9");
+            testInput.LinesToRead.Add("8");
+            testInput.LinesToRead.Add("7");
+
+            int getMoveTest = testBoard.GetMove(testInput);
+
+            Assert.AreEqual<int>(8, getMoveTest);
         }
     }
 }
